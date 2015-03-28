7c023be49de6e28e21cebcfd81392a5010e1d74d
jochambo <joey.chamberlin@gmail.com>
2015-03-28 09:39:21 -0500

    Added Ingredient and Recipe Models
    Created many-to-many relationship.

--- cookbook/models.py
+++ cookbook/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """
+    Has the text of the ingredient and a calorie_count.
+    """
+    title = models.CharField(max_length=200)
+    calorie_count = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Recipe(models.Model):
+    """
+    Has a title.
+    Is associated with a recipe.
+    """
+    title = models.CharField(max_length=200)
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __unicode__(self):
+        return self.title
