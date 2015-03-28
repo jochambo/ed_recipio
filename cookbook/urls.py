9a8e4f1a95d0e57eecf7782c3132c69f15d1c29f
jochambo <joey.chamberlin@gmail.com>
2015-03-28 09:55:27 -0500

    Added index view and routing.
    Added index template. Included routing in urls.py. Added initial view
    logic in views.py.

---
+++ cookbook/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from cookbook import views
+
+urlpatterns = patterns('',
+    # ex: /cookbook/
+    url(r'^$', views.index, name='index'),
+    # ex: /cookbook/5/
+    # url(r'^(?P<recipe_id>\d+)/$', views.detail, name='detail'),
+)
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from cookbook.models import Recipe, Ingredient
+
+
+def index(request):
+    recipe_list = Recipe.objects.all()
+    context = {'recipe_list': recipe_list}
+    return render(request, 'cookbook/index.html', context)
